1d2dd1d8cc7efa0ef0749c4d04a8a5d81bca96f4
nnnnadia <tristaonadia@gmail.com>
2022-11-27 16:38:40 -0300

    <sequelize> `models/`: create TeamModel

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,25 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class TeamModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
