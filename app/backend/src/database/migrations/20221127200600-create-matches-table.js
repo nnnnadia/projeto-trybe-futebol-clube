480840bf883c22b893dbb1bcb106dcf384c675d1
nnnnadia <tristaonadia@gmail.com>
2022-11-27 17:42:41 -0300

    <sequelize> `migrations/`: create matches table

---
+++ app/backend/src/database/migrations/20221127200600-create-matches-table.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      homeTeam: {
+        allowNull: false,
+        field: 'home_team',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      homeTeamGoals: {
+        allowNull: false,
+        field: 'home_team_goals',
+        type: Sequelize.INTEGER,
+      },
+      awayTeam: {
+        allowNull: false,
+        field: 'away_team',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        field: 'away_team_goals',
+        type: Sequelize.INTEGER,
+      },
+      inProgress: {
+        allowNull: false,
+        field: 'in_progress',
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
