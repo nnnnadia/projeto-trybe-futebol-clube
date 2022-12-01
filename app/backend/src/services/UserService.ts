92106da2f0ad88c76aebae31b34dea8aea56bb58
nnnnadia <tristaonadia@gmail.com>
2022-12-01 14:57:42 -0300

    <service> `UserService`: throws StatusError

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,15 +1,17 @@
 import AuthJWT from '../utils/AuthJWT';
 import UserModel from '../database/models/UserModel';
 import IUser from '../types/interfaces/IUser';
+import StatusError from '../utils/StatusError';
 
 export default class UserService {
   static findUserByEmail = async (email: string): Promise<IUser> => {
     try {
       const user = await UserModel.findOne({ where: { email } });
       if (user) return user;
-      throw new Error('USER_NOT_FOUND');
+      throw new StatusError(401, 'Incorrect email or password');
     } catch (error) {
-      throw new Error('INTERNAL_ERROR');
+      if (error instanceof StatusError) throw error;
+      throw new StatusError(500, 'Internal error');
     }
   };
 
