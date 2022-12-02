4939ad5f082ff265e5fcc1053daaff98b7206312
nnnnadia <tristaonadia@gmail.com>
2022-12-02 17:18:41 -0300

    <route> `/matches`: GET checks for inProgress query

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -3,6 +3,10 @@ import MatchController from '../controllers/MatchController';
 
 const match = Router();
 
-match.get('/', MatchController.findMatches);
+match.get(
+  '/',
+  MatchController.findMatches,
+  MatchController.findMatchesInProgress,
+);
 
 export default match;
