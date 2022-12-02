b97c3b25eefc7fcbcf9db60d70406a14de251fa6
nnnnadia <tristaonadia@gmail.com>
2022-12-02 12:09:19 -0300

    <route> `index.ts`: teamRoute routed

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import loginRoute from './loginRoute';
+import teamRoute from './teamRoute';
 import ErrorController from '../controllers/ErrorController';
 
 const router = Router();
 
 router.use('/login', loginRoute);
+router.use('/teams', teamRoute);
 
 router.use(ErrorController.handler);
 
