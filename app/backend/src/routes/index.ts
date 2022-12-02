0fc27040b2336954f07c5c6b8c824f7824481ac0
nnnnadia <tristaonadia@gmail.com>
2022-12-02 15:22:06 -0300

    <route> `index.ts`: matchRoute routed

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import loginRoute from './loginRoute';
 import teamRoute from './teamRoute';
+import matchRoute from './matchRoute';
 import ErrorController from '../controllers/ErrorController';
 
 const router = Router();
 
 router.use('/login', loginRoute);
 router.use('/teams', teamRoute);
+router.use('/matches', matchRoute);
 
 router.use(ErrorController.handler);
 
