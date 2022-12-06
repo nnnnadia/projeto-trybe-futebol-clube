74d30c30164c0bd9f4aa76aabb65220078eea41a
nnnnadia <tristaonadia@gmail.com>
2022-12-06 14:54:22 -0300

    <service> `TeamService`: method findEveryTeam and findTeamById (former findTeams)

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -3,12 +3,21 @@ import TeamModel from '../database/models/TeamModel';
 import StatusError from '../utils/StatusError';
 
 export default class TeamService {
-  static findTeams = async (id?: number): Promise<ITeam[] | ITeam> => {
+  static findEveryTeam = async (): Promise<ITeam[]> => {
     try {
-      const teams = id
-        ? await TeamModel.findOne({ where: { id } })
-        : await TeamModel.findAll();
+      const teams = await TeamModel.findAll();
       if (teams) return teams;
+      throw new StatusError(404, 'There is no team registered');
+    } catch (error) {
+      if (error instanceof StatusError) throw error;
+      throw new StatusError(500);
+    }
+  };
+
+  static findTeamById = async (id: number): Promise<ITeam> => {
+    try {
+      const team = await TeamModel.findOne({ where: { id } });
+      if (team) return team;
       throw new StatusError(404, 'There is no team with such id!');
     } catch (error) {
       if (error instanceof StatusError) throw error;
