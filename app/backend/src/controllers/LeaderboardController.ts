90a1ed5fab500d29f5ef92fe045c357ea05980df
nnnnadia <tristaonadia@gmail.com>
2022-12-08 15:11:05 -0300

    <controller> `LeaderboardController`: method getHomeTeamsLeaderboard and getAwayTeamsLeaderboard

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -2,11 +2,19 @@ import { Request, Response } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
-  static getLeaderboard = async (
+  static getHomeTeamsLeaderboard = async (
     _req: Request,
     res: Response,
   ) => {
-    const leaderboards = await LeaderboardService.getOrderedLeaderboards();
+    const leaderboards = await LeaderboardService.getOrderedLeaderboards(true, false);
+    res.status(200).json(leaderboards);
+  };
+
+  static getAwayTeamsLeaderboard = async (
+    _req: Request,
+    res: Response,
+  ) => {
+    const leaderboards = await LeaderboardService.getOrderedLeaderboards(false, true);
     res.status(200).json(leaderboards);
   };
 }
