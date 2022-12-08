c74368eff4e7d419b1b3f16d80bf2ea99568e544
nnnnadia <tristaonadia@gmail.com>
2022-12-08 15:10:45 -0300

    <service> `LeaderboardService`: now can get either home or away teams

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,17 +1,31 @@
 import TeamLeaderboard from './TeamLeaderboard';
 import MatchService from './MatchService';
+import IMatch from '../types/interfaces/IMatch';
 
 export default class LeaderboardService {
-  private static getHomeTeamsLeaderboard = async () => {
+  private static getTeamsId = (
+    matchesResults: IMatch[],
+    whichTeam: 'awayTeam' | 'homeTeam',
+  ): number[] => Array.from(new Set(matchesResults.map((result) => result[whichTeam])));
+
+  private static getTeamsLeaderboard = async (
+    getIt: boolean,
+    whichTeam: 'awayTeam' | 'homeTeam',
+  ) => {
+    if (!getIt) return [];
     const matchesResults = await MatchService.findMatchesInProgress(false);
-    const leaderboards = await Promise.all(Array.from(new Set(matchesResults
-      .map((result) => result.homeTeam)))
-      .map((id) => TeamLeaderboard.getTeamLeaderboard(id)));
+    const leaderboards = await Promise.all(
+      LeaderboardService.getTeamsId(matchesResults, whichTeam)
+        .map((id) => TeamLeaderboard.getTeamLeaderboard(id, whichTeam)),
+    );
     return leaderboards;
   };
 
-  static getOrderedLeaderboards = async () => {
-    const updatedLeaderboard = await LeaderboardService.getHomeTeamsLeaderboard();
+  static getOrderedLeaderboards = async (getHomeTeams: boolean, getAwayTeams: boolean) => {
+    const updatedLeaderboard = [
+      ...await LeaderboardService.getTeamsLeaderboard(getHomeTeams, 'homeTeam'),
+      ...await LeaderboardService.getTeamsLeaderboard(getAwayTeams, 'awayTeam'),
+    ];
     updatedLeaderboard.sort((a, b) => b.totalPoints - a.totalPoints
       || b.goalsBalance - a.goalsBalance
       || b.goalsFavor - a.goalsFavor
