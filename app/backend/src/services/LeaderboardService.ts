21bc64d751b18d9507c02a2626f1ece0a5932384
nnnnadia <tristaonadia@gmail.com>
2022-12-09 20:39:50 -0300

    <service> `LeaderboardService`: now calculates team points as home and as away team

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -16,20 +16,37 @@ export default class LeaderboardService {
     const matchesResults = await MatchService.findMatchesInProgress(false);
     const leaderboards = await Promise.all(
       LeaderboardService.getTeamsId(matchesResults, whichTeam)
-        .map((id) => TeamLeaderboard.getTeamLeaderboard(id, whichTeam)),
+        .map((id) => TeamLeaderboard.getParcialTeamLeaderboard(id, whichTeam)),
     );
     return leaderboards;
   };
 
+  private static getTeamsFullLeaderboard = async () => {
+    const matchesResults = await MatchService.findMatchesInProgress(false);
+    const leaderboards = await Promise.all(
+      LeaderboardService.getTeamsId(matchesResults, 'homeTeam')
+        .map((id) => TeamLeaderboard.getFullTeamLeaderboard(id)),
+    );
+    return leaderboards;
+  };
+
+  private static getUpdatedLeaderboards = async (getHomeTeams: boolean, getAwayTeams: boolean) => {
+    const updatedLeaderboard = getHomeTeams && getAwayTeams
+      ? LeaderboardService.getTeamsFullLeaderboard()
+      : [
+        ...await LeaderboardService.getTeamsLeaderboard(getHomeTeams, 'homeTeam'),
+        ...await LeaderboardService.getTeamsLeaderboard(getAwayTeams, 'awayTeam'),
+      ];
+    return updatedLeaderboard;
+  };
+
   static getOrderedLeaderboards = async (getHomeTeams: boolean, getAwayTeams: boolean) => {
-    const updatedLeaderboard = [
-      ...await LeaderboardService.getTeamsLeaderboard(getHomeTeams, 'homeTeam'),
-      ...await LeaderboardService.getTeamsLeaderboard(getAwayTeams, 'awayTeam'),
-    ];
-    updatedLeaderboard.sort((a, b) => b.totalPoints - a.totalPoints
+    const leaderboards = await LeaderboardService
+      .getUpdatedLeaderboards(getHomeTeams, getAwayTeams);
+    leaderboards.sort((a, b) => b.totalPoints - a.totalPoints
       || b.goalsBalance - a.goalsBalance
       || b.goalsFavor - a.goalsFavor
       || b.goalsOwn - a.goalsOwn);
-    return updatedLeaderboard;
+    return leaderboards;
   };
 }
