b05edb89f380c70090971410fab0dbd292f49da3
nnnnadia <tristaonadia@gmail.com>
2022-12-09 20:41:03 -0300

    <route> `/leaderboard`: GET

--- app/backend/src/routes/leaderboardRoute.ts
+++ app/backend/src/routes/leaderboardRoute.ts
@@ -3,6 +3,7 @@ import LeaderboardController from '../controllers/LeaderboardController';
 
 const leaderboard = Router();
 
+leaderboard.get('/', LeaderboardController.getFullTeamsLeaderboard);
 leaderboard.get('/home', LeaderboardController.getHomeTeamsLeaderboard);
 leaderboard.get('/away', LeaderboardController.getAwayTeamsLeaderboard);
 
